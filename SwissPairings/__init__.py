c39aa2e3a76e0d2acfa3b305bf0f012ff7d4e47b
Douglas Copas <douglas.copas@gmail.com>
2019-12-04 01:48:00 +0100

    parameterize url

--- SwissPairings/__init__.py
+++ SwissPairings/__init__.py
@@ -4,11 +4,14 @@ import bitarray
 from enum import Enum
 import azure.functions as func
 
-new_game_form = """<!DOCTYPE html>
+#base_url = "localhost:7071"
+base_url = "swisspairings.azurewebsites.net"
+
+new_game_form = f"""<!DOCTYPE html>
     <html>
     <body>
     <h2>New Game</h2>
-    <form action="https://swisspairings.azurewebsites.net/SwissPairings" method="POST">
+    <form action="http://{base_url}/SwissPairings" method="POST">
         Number of Players<br>
         <input type="text" name="pnum">
         <br>
@@ -118,7 +121,7 @@ class State:
                 self.players.append(player)   
             if self.played_rounds > 0:
                 width = get_player_width(self.number_of_players)
-                start_index = 17
+                start_index = 12
                 end_index = start_index + width
                 for _ in range(self.played_rounds):
                     for player in self.players:
@@ -199,7 +202,7 @@ class State:
 
         if self.played_rounds == 0: # create the ordered pairing list for the first round
             width = get_player_width(self.number_of_players)
-            start_index = 17
+            start_index = 12
             end_index = start_index + width
             for i in range(self.number_of_players):
                 player_string = ba_string[start_index:end_index]
@@ -368,7 +371,7 @@ def get_pairing_controls(state: State) -> str:
     <html>
     <body>
     <h2>Round {state.played_rounds + 1} : {state.state_string}</h2>
-    <form action="https://swisspairings.azurewebsites.net/SwissPairings/{state.state_string}" method="POST">
+    <form action="http://{base_url}/SwissPairings/{state.state_string}" method="POST">
         <br>"""
 
     pnum = len(state.ordered_pairing_list)
@@ -396,9 +399,9 @@ def get_pairing_controls(state: State) -> str:
     
 def get_header(pnum: int, rnum: int, rcurr: int, bye_player: bool) -> list:
 
-    pnum_header = [int(x) for x in '{:0{size}b}'.format(pnum,size=8)]
-    rnum_header = [int(x) for x in '{:0{size}b}'.format(rnum,size=4)]
-    rcurr_header = [int(x) for x in '{:0{size}b}'.format(rcurr,size=4)]
+    pnum_header = [int(x) for x in '{:0{size}b}'.format(pnum,size=5)]
+    rnum_header = [int(x) for x in '{:0{size}b}'.format(rnum,size=3)]
+    rcurr_header = [int(x) for x in '{:0{size}b}'.format(rcurr,size=3)]
     bye_bit = [1] if bye_player else [0]
     bit_list = pnum_header + rnum_header + rcurr_header + bye_bit
 
@@ -420,10 +423,10 @@ def get_player_width(pnum: int) -> int:
 
 def decode_header(ba_string: str):
     
-    number_of_players = int(ba_string[:8], 2)
-    number_of_rounds = int(ba_string[8:12] ,2)
-    played_rounds = int(ba_string[12:15] ,2)
-    bye_player = True if bool(int(ba_string[15:17])) else False
+    number_of_players = int(ba_string[:5], 2)
+    number_of_rounds = int(ba_string[5:8] ,2)
+    played_rounds = int(ba_string[8:11] ,2)
+    bye_player = True if bool(int(ba_string[11:12])) else False
     return number_of_players, number_of_rounds, played_rounds, bye_player
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -449,14 +452,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         if state_string is None:
             current_state = State(None)
             current_state.build_first_state_string(req.form)
-            headers = {"Location":  f"https://swisspairings.azurewebsites.net/SwissPairings/{current_state.state_string}"}
+            headers = {"Location":  f"http://{base_url}/SwissPairings/{current_state.state_string}"}
             return func.HttpResponse(status_code=302, headers=headers)
 
         else:
             current_state = State(state_string)
             current_state.update_history(req.form)
             current_state.build_new_state_string()
-            headers = {"Location":  f"https://swisspairings.azurewebsites.net/SwissPairings/{current_state.state_string}"}
+            headers = {"Location":  f"http://{base_url}/SwissPairings/{current_state.state_string}"}
             return func.HttpResponse(status_code=302, headers=headers)
 
     else:
